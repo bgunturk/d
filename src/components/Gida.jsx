c809da286f5b05d11ea25119d4c9c9f6b8a835cb
Bulent <71447509+bgunturk@users.noreply.github.com>
2023-10-16 17:22:14 +0300

    istibreacthaber

--- src/components/Gida.jsx
+++ src/components/Gida.jsx
@@ -1,11 +1,14 @@
+import styles from '../styles/gida.module.scss';
+
 const Gida = () => {
   return (
     
-        <div className="Gida">
-            <img src=''/>
-            <h1>Gida </h1>
-            <div className="header">Gida </div>
-            <div className="description">Gida </div>
+        <div className={styles.Gida}>
+            
+            <h1 className={styles.baslik}>Gida </h1>
+            <img className={styles.resim} src='https://www.istib.org.tr/resim/haberler/215_Ba%C5%9Fkan%20(Lansman)%20Tw.jpg'/>
+            <div className={styles.header}>İSTİB BAŞKANI ALİ KOPUZ: “OVP TARIM İÇİN UMUT VADEDİYOR” </div>
+            <div className={styles.description}>“OVP, Türkiye’nin tarımsal hasılasının artması, katma değerli ürünlerimizin geliştirilmesi ve ihracat hacmimizin yükseltilmesi için önemli adımlar barındırıyor"</div>
         </div>
   
   )
--- src/components/Teknoloji.jsx
+++ src/components/Teknoloji.jsx
@@ -1,11 +1,14 @@
+import styles from '../styles/teknoloji.module.scss';
+
 const Teknoloji = () => {
     return (
       
-          <div className="Teknoloji">
-              <img src=''/>
-              <h1 className="baslik">Teknoloji</h1>
-              <div className="header">Teknoloji</div>
-              <div className="description">Teknoloji</div>
+          <div className={styles.Teknoloji}>
+             
+              <h1 className={styles.baslik}>Teknoloji</h1>
+              <img className={styles.resim} src='https://www.istib.org.tr/resim/haberler/811_29720-3.jpg'/>
+              <div className={styles.header}>TOBB HEYETİ TOGG DENEYİM MERKEZİNDE</div>
+              <div className={styles.description}>İstanbul Ticaret TOBB Başkan Yardımcısı ve İSTİB Başkanı Ali Kopuz, Türkiye Odalar ve Borsalar Birliği (TOBB) Başkanı M. Rifat Hisarcıklıoğlu,</div>
           </div>
     
     )
